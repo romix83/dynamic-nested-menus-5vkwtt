c64242f8f3858f8388271104ec2bb0a1a667f214
romix83 <romix83@hotmail.com>
2020-11-07 21:24:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dynamic-nested-menus-5vkwtt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/dynamic-nested-menus-5vkwtt)
---
+++ app/app.component.ts
@@ -0,0 +1,98 @@
+import {Component} from '@angular/core';
+import {VERSION} from '@angular/material';
+import {NavItem} from './nav-item';
+
+@Component({
+  selector: 'material-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
+})
+export class AppComponent { 
+  version = VERSION;
+  navItems: NavItem[] = [
+    {
+      displayName: 'AngularMix',
+      iconName: 'close',
+      children: [
+        {
+          displayName: 'Speakers',
+          iconName: 'group',
+          children: [
+            {
+              displayName: 'Michael Prentice',
+              iconName: 'person',
+              route: 'michael-prentice',
+              children: [
+                {
+                  displayName: 'Delight your Organization',
+                  iconName: 'star_rate',
+                  route: 'material-design'
+                }
+              ]
+            },
+            {
+              displayName: 'Stephen Fluin',
+              iconName: 'person',
+              route: 'stephen-fluin',
+              children: [
+                {
+                  displayName: 'What\'s up with the Web?',
+                  iconName: 'star_rate',
+                  route: 'what-up-web'
+                }
+              ]
+            },
+            {
+              displayName: 'Mike Brocchi',
+              iconName: 'person',
+              route: 'mike-brocchi',
+              children: [
+                {
+                  displayName: 'My ally, the CLI',
+                  iconName: 'star_rate',
+                  route: 'my-ally-cli'
+                },
+                {
+                  displayName: 'Become an Angular Tailor',
+                  iconName: 'star_rate',
+                  route: 'become-angular-tailer'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          displayName: 'Sessions',
+          iconName: 'speaker_notes',
+          children: [
+            {
+              displayName: 'Delight your Organization',
+              iconName: 'star_rate',
+              route: 'material-design'
+            },
+            {
+              displayName: 'What\'s up with the Web?',
+              iconName: 'star_rate',
+              route: 'what-up-web'
+            },
+            {
+              displayName: 'My ally, the CLI',
+              iconName: 'star_rate',
+              route: 'my-ally-cli'
+            },
+            {
+              displayName: 'Become an Angular Tailor',
+              iconName: 'star_rate',
+              route: 'become-angular-tailer'
+            }
+          ]
+        },
+        {
+          displayName: 'Feedback',
+          iconName: 'feedback',
+          route: 'feedback'
+        }
+      ]
+    }
+  ];
+}
